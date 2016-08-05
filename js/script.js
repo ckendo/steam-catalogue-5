2bd6a91192b2cd885f2744c62b24c64b21603026
Kenji Endo <colvin.endo@gmail.com>
2016-08-04 21:32:29 -0700

    script moved to separate file, live naming actually works

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function getObjectInfo(){
+	// Get just <section class ="full"> elements
+	var sections = document.getElementsByClassName("full")
+	console.log(sections)
+	console.log(typeof(sections))
+	var array = Array.prototype.slice.call(sections)
+	console.log(array)
+	console.log(typeof(array))
+	console.log(array.length)
+	var objectInfo = []
+	for (var i = 0; i < array.length; i++){
+		var currName = array[i].id.replace('-', ' ')
+		var currTop = Math.round(array[i].getBoundingClientRect().top)
+		var object = {name: currName, top: currTop}
+		objectInfo.push(object)
+	}
+	// Ensure sorted by increasing y value
+	objectInfo = objectInfo.sort(function(a, b){
+		return a.top - b.top
+	})
+	return objectInfo
+}
+
+var objectInfo = getObjectInfo()
+
+window.onscroll = function showSectionName(){
+	var offset = window.innerHeight/6
+	// var objectInfo = this.getObjectInfo()
+	var curr = window.pageYOffset
+	// No need to binary search, probably fast enough
+	for (var i = 0; i < objectInfo.length; i++){
+		if (curr > objectInfo[i].top - offset){
+			document.getElementById("updateText").innerHTML = objectInfo[i].name	
+		}
+	}	
+}
