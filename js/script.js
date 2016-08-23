31adacfe9938132c9dac1fe99a4a389e45f99833
Kenji Endo <colvin.endo@gmail.com>
2016-08-23 14:59:49 -0400

    fix javascript get position
    now works on resize and on hash change (seemingly)

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
+// var globalObjectInfo; // = getObjectInfo()
+
+window.addEventListener("resize", myFunction)
+
 function getObjectInfo(){
+	console.log('recalculating')
 	// Get just <section class ="full"> elements
 	var sections = document.getElementsByClassName("full")
-	console.log(sections)
-	console.log(typeof(sections))
+	// console.log(sections)
+	// console.log(typeof(sections))
 	var array = Array.prototype.slice.call(sections)
-	console.log(array)
-	console.log(typeof(array))
-	console.log(array.length)
+	// console.log(array)
+	// console.log(typeof(array))
+	// console.log(array.length)
 	var objectInfo = []
 	for (var i = 0; i < array.length; i++){
-		console.log('orig:', array[i].id)
+		// console.log('orig:', array[i].id)
 		// var currName = array[i].id.replace('-', ' ')
 		var currName = replaceAll(array[i].id, '-', ' ')
-		console.log('replaced:', currName)
-		var currTop = Math.round(array[i].getBoundingClientRect().top)
+		// console.log('replaced:', currName)
+		// var currTop = Math.round(array[i].getBoundingClientRect().top)
+		var el = array[i]
+		console.log(typeof(el))
+		var currTop = offset(el).top
 		var object = {name: currName, top: currTop, orig: array[i].id}
 		objectInfo.push(object)
 	}
@@ -21,7 +29,31 @@ function getObjectInfo(){
 	objectInfo = objectInfo.sort(function(a, b){
 		return a.top - b.top
 	})
+	// globalObjectInfo = objectInfo
 	return objectInfo
+}
+//Initial 
+var objectInfo = getObjectInfo()
+
+window.onresize=function(){
+	objectInfo = getObjectInfo()
+}
+
+window.onhashchange=function(){
+	objectInfo = getObjectInfo()
+}
+
+function offset(el) {
+	var rect = el.getBoundingClientRect(),
+	scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+	scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+
+var x = 0;
+function myFunction() {
+	var txt = x += 1;
+    console.log(txt)
 }
 
 function replaceAll(str, find, replace) {
@@ -36,9 +68,8 @@ function replaceAll(str, find, replace) {
 	return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 }
 
-var objectInfo = getObjectInfo()
-
 window.onscroll = function showSectionName(){
+
 	var offset = window.innerHeight/4.5
 	// var objectInfo = this.getObjectInfo()
 	var curr = window.pageYOffset
