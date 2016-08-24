426281df98a27c002a58c966c7a9ec693bacad94
Kenji Endo <colvin.endo@gmail.com>
2016-08-23 22:20:23 -0400

    color bars, TODO: read more/less height calculation

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ function getObjectInfo(){
 	// console.log(array.length)
 	var objectInfo = []
 	for (var i = 0; i < array.length; i++){
+		console.log('id:', array[i].id)
 		// console.log('orig:', array[i].id)
 		// var currName = array[i].id.replace('-', ' ')
 		var currName = replaceAll(array[i].id, '-', ' ')
@@ -53,7 +54,7 @@ function offset(el) {
 var x = 0;
 function myFunction() {
 	var txt = x += 1;
-    console.log(txt)
+	console.log(txt)
 }
 
 function replaceAll(str, find, replace) {
@@ -77,8 +78,82 @@ window.onscroll = function showSectionName(){
 	for (var i = 0; i < objectInfo.length; i++){
 		if (curr > objectInfo[i].top - offset){
 			document.getElementById("updateText").innerHTML = objectInfo[i].name
-			// document.getElementById("fixed-nav-bar").classList.add("test")
-			document.getElementById("fixed-nav-bar").style.borderTopColor = "red"
+			var colorInfo = getColors(objectInfo[i].orig)
+			document.querySelector("html").style.setProperty("--color-one", colorInfo[0]);
+			document.querySelector("html").style.setProperty("--color-two", colorInfo[1]);
+			document.querySelector("html").style.setProperty("--color-three", colorInfo[2]);
+			document.querySelector("html").style.setProperty("--color-four", colorInfo[3]);
 		}
 	}	
+}
+
+function getColors(name){
+	return colorDict[articleDict[name]]
+}
+
+/* Hardcoded dict unless you think of a better way */
+
+var articleDict = {
+	'Welcome': 'standard',
+	'Catalogue-Five': 'standard',
+	'Contents': 'standard',
+	'Foreword': 'standard',
+	'Hello-From': 'standard',
+	'Rutgers-University': 'rutgers',
+	'Boston-University': 'bu',
+	'Harvard-University': 'harvard',
+	'The-New-School': 'new-school',
+	'University-of-Michigan': 'michigan',
+	'University-of-Ottawa': 'ottawa',
+	'MIT': 'mit',
+	'Yale': 'yale',
+	'Meet-Me-in-the-Middle': 'brown',
+	'Time-Well-Spent': 'risd',
+	'Mushroom-Hunt': 'risd-brown',
+	'Laurence-Humier': 'risd',
+	'Weather-or-Not?': 'risd-brown',
+	'GENSPACE': 'risd',
+	'Citizen-+-Virtual': 'risd-brown-mit-bu',
+	'ADAPTATION': 'standard',
+	'Gingko-Bioworks': 'risd-brown',
+	'Brainy-Beats': 'brown',
+	'RIGAMAJIG': 'risd',
+	'STEAM-Assistive-Makeathon': 'brown',
+	'STEAM-Week': 'risd',
+	'DISCIPLINARYISM': 'standard',
+	'Worlds-From-Cardboard': 'risd-brown',
+	'Yeast-Selfies': 'risd-brown',
+	'Looking-Forward-and-Ahead': 'standard',
+	'We-Are': 'standard',
+	'Colophon': 'standard',
+}
+
+var colorDict = {
+	/* Nav bar styles*/
+	// Default
+	'standard': ['#000000', '#000000', '#000000', '#000000'],
+	// Rutgers
+	'rutgers': ['#6048a8', '#6048a8', '#6048a8', '#6048a8'],
+	// BU
+	'bu': ['#0ba016', '#0ba016', '#0ba016', '#0ba016'], 
+	// Harvard
+	'harvard': ['#fcc342', '#fcc342', '#fcc342', '#fcc342'],
+	// New School
+	'new-school': ['#f77116', '#f77116', '#f77116', '#f77116'],
+	// Michigan
+	'michigan': ['#000000', '#000000', '#000000', '#000000'],
+	// Ottawa
+	'ottawa': ['#000000', '#000000', '#000000', '#000000'],
+	// MIT
+	'mit': ['#24A8B3', '#24A8B3', '#24A8B3', '#24A8B3'], 
+	// Yale
+	'yale': ['#1A76BB', '#1A76BB', '#1A76BB', '#1A76BB'],
+	// Brown
+	'brown': ['#EC1C24', '#EC1C24', '#EC1C24', '#EC1C24'],
+	// RISD
+	'risd': ['#737373', '#737373', '#737373', '#737373'],
+	// RISD+Brown
+	'risd-brown': ['#737373', '#737373', '#EC1C24', '#EC1C24'],
+	// RISD+Brown+MIT+BU
+	'risd-brown-mit-bu': ['#737373', '#EC1C24', '#24A8B3', '#0ba016'],
 }
